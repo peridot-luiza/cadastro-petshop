5bcc2f53c90813fbc9582ecd1258580ce3ba4285
peridot <you@example.com=luizaestefane7@gmail.com>
2025-08-26 21:00:14 -0300

    feat: Sincroniza arquivos com o GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 // vite.config.ts
 // Este ficheiro configura o Vite para o seu projecto.
+// vite.config.ts
+// Este ficheiro configura o Vite para o seu projecto.
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // Define a base URL para o seu projecto, o que é crucial para o GitHub Pages.
-  // A URL deve ser o nome do seu repositório no GitHub.
-  base: '/cadastro-petshop/',
+// Define a base URL para o seu projecto, o que é crucial para o GitHub Pages.
+// A URL deve ser o nome do seu repositório no GitHub.
+base: '/cadastro-petshop/',
   server: {
     host: "::",
     port: 8080,
